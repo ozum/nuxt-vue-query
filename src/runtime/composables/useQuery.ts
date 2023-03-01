eb4aef255c28a27ae86b494a2f22fc8cef953a86
Özüm Eldoğan <ozum@ozum.net>
2023-03-01 18:41:43 +0300

    fix: use useFetch instead of $fetch

--- src/runtime/composables/useQuery.ts
+++ src/runtime/composables/useQuery.ts
@@ -1,4 +1,5 @@
-import { unref, computed, ComputedRef } from "vue";
+import { unref, computed, ComputedRef, onServerPrefetch } from "vue";
+import { useLazyFetch } from "#imports";
 import type { RouterMethod } from "h3";
 import { useQuery } from "@tanstack/vue-query";
 import type { UseQueryOptions, QueryMeta } from "@tanstack/vue-query";
@@ -12,20 +13,24 @@ interface Meta<M extends RouterMethod, R extends Request<M>> extends QueryMeta {
   url: string;
   query?: Query<R, M>;
   method: M;
+  headers?: Record<string, string>;
 }
 
 interface QueryOptions<X, Y, Z, QKP extends any[]> extends UseQueryOptions<X, Y, Z> {
   queryKeyPrefix?: QKP;
+  headers?: Record<string, string>;
 }
 
 type QueryKey<M extends RouterMethod, QKP extends any[], R extends Request<M>> = [...QKP, M, ...string[], Query<R, M> | undefined];
 
-const queryFn = function queryFn<M extends RouterMethod, R extends Request<M>>({ meta }: { meta: Meta<M, R> }): Response<R, M>  {
-  const { method, url, query } = meta;
-  return $fetch(unref(url), { query, method }) as Response<R, M>;
+async function queryFn<M extends RouterMethod, R extends Request<M>>({ meta }: { meta: Meta<M, R> }): Promise<Response<R, M>>  {
+  const { method, url, query, headers } = meta;
+  const { data, error } = await useLazyFetch<Response<R, M>>(unref(url), { query, method, headers });
+  if (error.value) throw error.value;
+  return data.value;
 }
 
-export async function useApiGet<
+export function useApiGet<
   TError = unknown,
   R extends Request<"get"> = Request<"get">,
   TQueryFnData = Response<R, "get">,
@@ -35,9 +40,9 @@ export async function useApiGet<
   const urlAddress =  computed (() => getUrl(url));
   const prefix = (options?.queryKeyPrefix || []) as QKP
   const queryKey = computed(() => [...prefix, "get", ...urlAddress.value.split("/").slice(1), query]) as ComputedRef<QueryKey<"get", QKP, R>>;
-  const meta = computed(() => ({ ...options?.meta, url: urlAddress.value, query, method: "get" })) as ComputedRef<Meta<"get", R>>;
+  const meta = computed(() => ({ ...options?.meta, url: urlAddress.value, query, method: "get", headers: options?.headers })) as ComputedRef<Meta<"get", R>>;
   const mergedOptions = { queryKey, queryFn: queryFn as any, ...options, meta };
   const result = useQuery(mergedOptions);
-  if (typeof result.suspense == "function") await result.suspense();
+  onServerPrefetch(async () => await result.suspense()); // For Nuxt SSR.
   return { ...result, queryKey }; // Promise<UseQueryDefinedReturnType<TData, TError> & { queryKey: QueryKey<R> }>
 }
